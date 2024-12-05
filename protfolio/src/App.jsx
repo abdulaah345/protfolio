9af475c341a890bd6f26b6935e4658d0f2868144
abdullah345 <abdallahmohamed54655@gmail.com>
2024-12-05 19:27:23 +0200

    v9

--- protfolio/src/App.jsx
+++ protfolio/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Banner from "./component/Banner";
+import Skills from "./component/Skills";
 
 function App() {
   return (
     <div className="App">
       <Banner />
+      <Skills/>
     </div>
   );
 }
