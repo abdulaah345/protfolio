b9157309651928004b35e5f0cf864fb8b308c123
mido01234 <ahmedmokhles95@gmail.com>
2024-12-06 01:02:28 +0200

    projects

--- protfolio/src/App.jsx
+++ protfolio/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Banner from "./component/Banner";
+import Projects from "./component/Projects";
 
 function App() {
   return (
     <div className="App">
       <Banner />
+      <Projects/>
     </div>
   );
 }
